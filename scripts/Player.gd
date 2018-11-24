5dd75a89ff90f0a025670d5167a19bb899fdfc33
SyntaX <michael.leithner@gmail.com>
2018-11-24 17:33:11 +0100

    Revert "Merge branch 'master' of https://github.com/incredibleLeitman/ITP3-ProceduRally"
    This reverts commit e6cb57adda1346c02b1ee6893c834cd71680abcf, reversing
    changes made to a0360205d1d801604e7872804a71eabc42a91bf1.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,11 +1,8 @@
-extends KinematicBody
+extends Spatial
 
-onready var pull_ray_down = get_node("CollisionRays/Down")
-onready var repel_ray_down = get_node("RepelRays/Down")
+onready var rays = get_node("CollisionRays")
 
-var pull_speed = 12
-var move_speed = Vector3(20, 0, 8)
-var rot_speed = 20
+var col_speed = 3
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -13,43 +10,32 @@ func _ready():
 	pass
 
 func _process(delta):
-	# Handle rotation
-	var rot = 0
-	var movement = Vector3()
+	var direction = Vector3(0, 0, 0)
 	
+	for ray in rays.get_children():
+		if ray.is_colliding():
+			
+			var weighted_distance = (7 - ray.get_collision_point().distance_to(translation)) / 7
+			direction += ray.cast_to.normalized() * weighted_distance * delta * col_speed
+		
+	translation += direction
+	
+	var rot = 0;
 	if Input.is_action_pressed("move_left"):
-		#rot -= delta * rot_speed
-		movement.x += -move_speed.z # TODO weird coordinates
+		rot += delta * 100
 		
 	if Input.is_action_pressed("move_right"):
-		#rot += delta * rot_speed
-		movement.x += move_speed.z
+		rot -= delta * 100
 
-	rotation_degrees.x += rot;
+	# cap at 90 °
+	var new_rot_z = rotation_degrees.z + rot
+	#if new_rot_z < 90 && new_rot_z > -90:
+	if new_rot_z < 45 && new_rot_z > -45:
+		rotation_degrees.z += rot;
+	print("degrees.z:" + str(rotation_degrees.z))
 
-	# Handle movement
-	if Input.is_action_pressed("move_backward"):
-		movement.z -= -move_speed.x # TODO weird coordinates
-		
 	if Input.is_action_pressed("move_forward"):
-		movement.z += -move_speed.x # TODO weird coordinates
+		translation -= Vector3(0, 0, delta * 100)
 		
-	# Handle gravity
-	var pull_direction = Vector3()
-	var pull_force = 0
-	
-	if pull_ray_down.is_colliding():
-		# Get the pull force based on the difference of target and current collision point
-		pull_direction = pull_ray_down.cast_to.normalized()
-		pull_force = pull_ray_down.get_collision_point().distance_to(translation) - repel_ray_down.cast_to.length()
-		pull_force *= pull_speed
-		
-		# Calculate upwards direction using the collision normal
-		var normal = pull_ray_down.get_collision_normal()
-		var diff = (normal - transform.basis.y)
-		
-		# Turn more the greater the difference, therefore multiply with length
-		transform.basis.y += diff * delta * rot_speed * diff.length()
-		
-	# Apply
-	move_and_slide(pull_direction * pull_force + movement)
+	if Input.is_action_pressed("move_backward"):
+		translation += Vector3(0, 0, delta * 100)
