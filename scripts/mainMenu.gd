c8e4d1e82d7246c06e743ddd3ecaef4fc701d1f9
Ententerminator <zuckaboy@gmail.com>
2018-12-14 17:50:25 +0100

    great non-functioning buttons added to main menu

--- scripts/mainMenu.gd
+++ scripts/mainMenu.gd
@@ -3,6 +3,7 @@ extends Node
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
+var menuNavigation = 0;
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -14,10 +15,63 @@ func _ready():
 #	# Update game logic here.
 #	pass
 
-
-func _on_Button2_pressed():
+func _on_exit_pressed():
 	get_tree().quit()
 
 
-func _on_Button_pressed():
+func _on_start_game_pressed():
 	get_tree().change_scene("res://scenes/World.tscn")
+
+
+
+func _on_single_player_pressed():
+	menuNavigation = 1
+	hide_show()
+	
+func _on_multi_player_pressed():
+	menuNavigation = 4
+	hide_show()
+
+
+func _on_host_pressed():
+	menuNavigation = 2
+	hide_show()
+
+
+func _on_join_pressed():
+	menuNavigation = 3
+	hide_show()
+
+
+func hide_show():
+	var hideAllChildren = get_node("Container/HBoxContainer/VBoxContainer").get_children()
+	for i in hideAllChildren:
+		i.hide()
+	
+	if(menuNavigation == 0):
+		get_node("Container/HBoxContainer/VBoxContainer/single_player").show()
+		get_node("Container/HBoxContainer/VBoxContainer/multi_player").show()
+		get_node("Container/HBoxContainer/VBoxContainer/exit").show()
+	elif(menuNavigation == 1 || menuNavigation == 2):
+		get_node("Container/HBoxContainer/VBoxContainer/start_game").show()
+		get_node("Container/HBoxContainer/VBoxContainer/song_selection").show()
+		get_node("Container/HBoxContainer/VBoxContainer/song_duration").show()
+		get_node("Container/HBoxContainer/VBoxContainer/back").show()
+	elif(menuNavigation == 3):
+		get_node("Container/HBoxContainer/VBoxContainer/start_game").show()
+		get_node("Container/HBoxContainer/VBoxContainer/back").show()
+	elif(menuNavigation == 4):
+		get_node("Container/HBoxContainer/VBoxContainer/host").show()
+		get_node("Container/HBoxContainer/VBoxContainer/join").show()
+		get_node("Container/HBoxContainer/VBoxContainer/back").show()
+		
+
+func _on_back_pressed():
+	
+	if(menuNavigation == 1 || menuNavigation == 4):
+		menuNavigation = 0
+		hide_show()
+	elif(menuNavigation == 2 || menuNavigation == 3):
+		menuNavigation = 4
+		hide_show()
+
